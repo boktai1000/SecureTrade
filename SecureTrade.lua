320cac27f2db2460ec806f91f91426c2bf6ecad3
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2021-08-27 20:35:39 -0500

    Add files via upload

---
+++ SecureTrade.lua
@@ -0,0 +1,24 @@
+local f = CreateFrame("FRAME")
+
+local function OnUpdate(self)
+	if not self.enableAt then return end
+	local t = GetTime()
+	if t < self.enableAt then
+		TradeFrameTradeButton:Disable()
+		TradeFrameTradeButton:SetText(string.format("%.1fs", self.enableAt - t))
+		return
+	end
+	self.enableAt = nil
+	TradeFrameTradeButton:Enable()
+	TradeFrameTradeButton:SetText("Trade")
+	self:SetScript("OnUpdate", nil)
+end
+
+f:RegisterEvent("TRADE_TARGET_ITEM_CHANGED")
+f:RegisterEvent("TRADE_MONEY_CHANGED")
+f:SetScript("OnEvent", function(self)
+	TradeFrameTradeButton:Disable()
+	self.enableAt = GetTime() + 3
+	OnUpdate(self)
+	self:SetScript("OnUpdate", OnUpdate)
+end)
